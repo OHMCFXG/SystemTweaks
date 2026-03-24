52137fe575a3a64013a78f454372a1fcc8526e97
OHMCFXG <jianghaobing3690@gmail.com>
2026-03-24 22:28:32 +0800

    Enable dynamic color and update Compose stack

--- app/src/main/java/moe/ice/systemtweaks/ui/theme/Theme.kt
+++ app/src/main/java/moe/ice/systemtweaks/ui/theme/Theme.kt
@@ -3,8 +3,11 @@ package moe.ice.systemtweaks.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = AquaSoft,
@@ -39,9 +42,16 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun SystemTweaksTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    val context = LocalContext.current
+    val colorScheme = when {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(context)
+        dynamicColor -> dynamicLightColorScheme(context)
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
 
     MaterialTheme(
         colorScheme = colorScheme,
